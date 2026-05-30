596c9382a57a94fd93a25ad642601e7d943d1f5d
mehedi <mahbub@evidentbd.com>
2026-05-30 13:05:57 +0600

    update auth

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import AdminLoginView, ProfileView
 
 urlpatterns = [
+    path('auth/login/', AdminLoginView.as_view(), name='login'),
     path('auth/admin/login/', AdminLoginView.as_view(), name='admin-login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
